6ba7d76d76cc02264f3bb9203688c6bde5c4f9cd
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2017-06-27 14:24:09 +0300

    C2 work processing queue
    Change-Id: Id59f780800f4dbf237631351cd7cab4d83df6c4a

---
+++ c2_utils/include/mfx_cmd_queue.h
@@ -0,0 +1,72 @@
+/********************************************************************************
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+*********************************************************************************/
+
+#pragma once
+
+#include <C2Work.h>
+
+#include <functional>
+#include <queue>
+#include <thread>
+
+#include "mfx_defs.h"
+#include "mfx_debug.h"
+
+// queue executing commands in dedicated thread
+class MfxCmdQueue
+{
+public:
+    MfxCmdQueue() = default;
+    MFX_CLASS_NO_COPY(MfxCmdQueue);
+
+public:
+    void Start();
+
+    void Stop();
+
+    void Abort();
+
+    template<class Task>
+    void Push(Task&& task);
+
+private:
+    typedef std::function<void()> MfxCmd;
+
+private:
+    void WaitingPop(MfxCmd* command);
+
+    void Shutdown(bool abort);
+
+    void Process();
+
+private:
+    std::mutex mutex_; // push/pop protection
+    std::queue<MfxCmd> data_;
+    std::condition_variable condition_; // event from push to processor
+    std::thread working_thread_;
+    std::mutex shutdown_mutex_; // protect if shutdown is requested simultaneously from some threads
+};
+
+template<class Task>
+void MfxCmdQueue::Push(Task&& task)
+{
+    MFX_DEBUG_TRACE_FUNC;
+
+    // incoming task could be a lambda move assignable, but not copy assignable
+    // such lambdas aren't convertible to std::function stored in our queue
+    // so we create copy assignable lambda - it captures shared_ptr to incoming task
+    std::shared_ptr<Task> shared_task = std::make_shared<Task>(std::move(task));
+
+    MfxCmd cmd = [shared_task] () { (*shared_task)(); }; // just call task
+
+    std::lock_guard<std::mutex> lock(mutex_);
+    data_.push(cmd);
+    condition_.notify_one();
+}
---
+++ c2_utils/src/mfx_cmd_queue.cpp
@@ -0,0 +1,77 @@
+/********************************************************************************
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2017 Intel Corporation. All Rights Reserved.
+
+*********************************************************************************/
+
+#include "mfx_cmd_queue.h"
+#include "mfx_debug.h"
+
+void MfxCmdQueue::Start()
+{
+    MFX_DEBUG_TRACE_FUNC;
+    working_thread_ = std::thread(std::bind(&MfxCmdQueue::Process, this));
+}
+
+void MfxCmdQueue::Stop()
+{
+    MFX_DEBUG_TRACE_FUNC;
+    bool abort = false;
+    Shutdown(abort);
+}
+
+void MfxCmdQueue::Abort()
+{
+    MFX_DEBUG_TRACE_FUNC;
+    bool abort = true;
+    Shutdown(abort);
+}
+
+void MfxCmdQueue::WaitingPop(MfxCmd* command)
+{
+    MFX_DEBUG_TRACE_FUNC;
+    std::unique_lock<std::mutex> lock(mutex_);
+    condition_.wait(lock, [this] { return !data_.empty(); });
+    *command = data_.front();
+    data_.pop();
+}
+
+void MfxCmdQueue::Shutdown(bool abort)
+{
+    MFX_DEBUG_TRACE_FUNC;
+    {
+        std::lock_guard<std::mutex> lock(mutex_);
+        if(abort) {
+            data_ = std::queue<MfxCmd>();
+        }
+        data_.push(MfxCmd()); // nullptr command is a stop thread command
+        condition_.notify_one();
+    }
+    {
+        // mutexed code section to not have exception in join
+        // if already joined in another thread or not started
+        std::lock_guard<std::mutex> lock(shutdown_mutex_);
+        if(working_thread_.joinable()) {
+            working_thread_.join();
+        }
+    }
+}
+
+void MfxCmdQueue::Process()
+{
+    MFX_DEBUG_TRACE_FUNC;
+    for(;;) {
+        MfxCmd mfx_cmd;
+        WaitingPop(&mfx_cmd);
+        if(mfx_cmd == nullptr) {
+            break;
+        }
+        else {
+            mfx_cmd();
+        }
+    }
+}
