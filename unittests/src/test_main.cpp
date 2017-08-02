1851318d49a20977629774c5ad68589b951c08b3
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2017-08-02 17:51:58 +0300

    Added dump output option to unittests
    Change-Id: If053f133ad8e78260a637313d5d9de1a3a742e76

--- unittests/src/test_main.cpp
+++ unittests/src/test_main.cpp
@@ -9,9 +9,30 @@ Copyright(c) 2017 Intel Corporation. All Rights Reserved.
 *********************************************************************************/
 
 #include "gtest_emulation.h"
+#include "test_components.h"
+
+#include <iostream>
 
 int main(int argc, char** argv) {
 
     ::testing::InitGoogleTest(&argc, argv);
+
+    // gtest doesn't provide any support to custom command line options,
+    // they should be processed after gtest processed.
+    bool dump_output = false;
+
+    for(int i = 1; i < argc; ++i) {
+
+        std::string arg(argv[i]);
+
+        if(arg == "--dump-output") {
+            dump_output = true;
+        } else {
+            std::cout << "Unexpected argument: " << arg << std::endl;
+        }
+    }
+
+    BinaryWriter::Enable(dump_output);
+
     return RUN_ALL_TESTS();
 }
