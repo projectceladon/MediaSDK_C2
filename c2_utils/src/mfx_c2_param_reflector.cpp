a49d954babf8828510c94e1101d6e836f7572b81
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2018-04-26 12:23:48 +0300

    [c2] Get rid of _C2FieldId and C2FieldDescriptor offset() and size() getters (header customization)
    Issue: MDP-39733
    Change-Id: I4beeb888635f0176f54b5dd54f24be4ad9fa8ba8

--- c2_utils/src/mfx_c2_param_reflector.cpp
+++ c2_utils/src/mfx_c2_param_reflector.cpp
@@ -12,9 +12,41 @@ Copyright(c) 2017-2018 Intel Corporation. All Rights Reserved.
 #include "mfx_debug.h"
 #include "mfx_c2_utils.h"
 #include <sstream>
-#include <C2ParamInternal.h>
 
-using namespace android;
+// Google uses a trick to access private fields/methods of some classes
+// like C2ParamField and C2FieldDescriptor: it declares _C2ParamInspector
+// as friend for those private classes, then define _C2ParamInspector in
+// internal header - C2ParamInternal.h.
+// Unfortunately Google's _C2ParamInspector is not enough for out needs:
+// it gives access to C2ParamField, but doesn't help with C2FieldDescriptor.
+// So we have to define our own _C2ParamInspector for everything we need.
+struct _C2ParamInspector
+{
+   inline static uint32_t getIndex(const C2ParamField &pf) {
+        return pf._mIndex;
+    }
+
+    inline static uint32_t getOffset(const C2ParamField &pf) {
+        return pf._mFieldId._mOffset;
+    }
+
+    inline static uint32_t getSize(const C2ParamField &pf) {
+        return pf._mFieldId._mSize;
+    }
+
+    inline static
+    C2ParamField CreateParamField(C2Param::Index index, uint32_t offset, uint32_t size) {
+        return C2ParamField(index, offset, size);
+    }
+    // here and below is what Google is missing
+    inline static uint32_t getOffset(const C2FieldDescriptor& fd) {
+        return fd._mFieldId._mOffset;
+    }
+
+    inline static uint32_t getSize(const C2FieldDescriptor& fd) {
+        return fd._mFieldId._mSize;
+    }
+};
 
 bool MfxC2ParamReflector::ValidateParam(const C2Param* param,
     std::vector<std::unique_ptr<C2SettingResult>>* const failures)
@@ -46,7 +78,7 @@ bool MfxC2ParamReflector::ValidateParam(const C2Param* param,
                     // So sizeof(C2Param) should be added to offset before search.
 
                     C2ParamField param_field = _C2ParamInspector::CreateParamField(param->index(),
-                        field_desc.offset() + sizeof(C2Param), sizeof(uint32_t));
+                        _C2ParamInspector::getOffset(field_desc) + sizeof(C2Param), sizeof(uint32_t));
 
                     MFX_DEBUG_TRACE_STREAM("Looking for C2ParamField:"
                         << std::hex << " index: " << _C2ParamInspector::getIndex(param_field)
@@ -61,7 +93,7 @@ bool MfxC2ParamReflector::ValidateParam(const C2Param* param,
                                 uint32_t min = supported.range.min.u32;
                                 uint32_t max = supported.range.max.u32;
                                 const uint8_t* ptr_val = (const uint8_t*)param
-                                    + sizeof(C2Param) + field_desc.offset();
+                                    + sizeof(C2Param) + _C2ParamInspector::getOffset(field_desc);
                                 const uint32_t val = *(const uint32_t*)ptr_val;
 
                                 if(val < min || val > max) {
@@ -166,7 +198,8 @@ void MfxC2ParamReflector::DumpParams()
         for(const auto& field_desc : pair.second) {
             std::ostringstream oss;
             oss << indent << field_desc.name() << " " << field_desc.type()
-                << " " << field_desc.offset() << " " << field_desc.size();
+                << " " << _C2ParamInspector::getOffset(field_desc)
+                << " " << _C2ParamInspector::getSize(field_desc);
             MFX_DEBUG_TRACE_MSG(oss.str().c_str());
         }
     }
@@ -177,15 +210,9 @@ void MfxC2ParamReflector::DumpParams()
 
         const C2ParamField& param_field = pair.first;
 
-        struct C2ParamFieldCover {
-            uint32_t index;
-            _C2FieldId field_id;
-        };
-        const C2ParamFieldCover* cover = (const C2ParamFieldCover*)&param_field;
-
-        oss << "ParamID:" << std::hex << cover->index;
-        oss << " offset:" << std::dec << cover->field_id.offset();
-        oss << " size:" << cover->field_id.size();
+        oss << "ParamID:" << std::hex << _C2ParamInspector::getIndex(param_field);
+        oss << " offset:" << std::dec << _C2ParamInspector::getOffset(param_field);
+        oss << " size:" << _C2ParamInspector::getSize(param_field);
 
         const C2FieldSupportedValues& values = pair.second;
 
