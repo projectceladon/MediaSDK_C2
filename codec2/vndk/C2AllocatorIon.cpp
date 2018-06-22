cce7672fcd9ad1ab362635dbd482a1ea32fd174e
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2018-06-22 14:36:24 +0300

    [c2][vndk][fix-google] Linear allocator uses std::vector for allocation if ion is unavailable
    Issue: MDP-48133
    
    Test: not run
    
    (cherry picked from commit 440bd49ced4500f34130512c153917367d848c70 without changes)
    
    Change-Id: I38cd071e0180c305b3122b403057d1715e74035f

--- codec2/vndk/C2AllocatorIon.cpp
+++ codec2/vndk/C2AllocatorIon.cpp
@@ -177,6 +177,7 @@ private:
             }
             // C2_CHECK(bufferFd < 0);
             // C2_CHECK(buffer < 0);
+            mVector.resize(capacity);
         }
     }
 
@@ -231,6 +232,11 @@ public:
     c2_status_t map(size_t offset, size_t size, C2MemoryUsage usage, C2Fence *fence, void **addr) {
         (void)fence; // TODO: wait for fence
         *addr = nullptr;
+
+        if (mIonFd < 0) {
+            *addr = mVector.data() + offset;
+            return C2_OK;
+        }
         if (!mMappings.empty()) {
             ALOGV("multiple map");
             // TODO: technically we should return DUPLICATE here, but our block views don't
@@ -289,6 +295,9 @@ public:
     }
 
     c2_status_t unmap(void *addr, size_t size, C2Fence *fence) {
+        if (mIonFd < 0) {
+            return C2_OK;
+        }
         if (mMapFd < 0 || mMappings.empty()) {
             ALOGD("tried to unmap unmapped buffer");
             return C2_NOT_FOUND;
@@ -335,7 +344,7 @@ public:
     }
 
     c2_status_t status() const {
-        return mInit;
+        return C2_OK/*mInit*/;
     }
 
     const C2Handle *handle() const {
@@ -363,6 +372,7 @@ private:
         size_t size;
     };
     std::list<Mapping> mMappings;
+    std::vector<uint8_t> mVector;
 };
 
 c2_status_t C2AllocationIon::map(
@@ -454,7 +464,7 @@ void C2AllocatorIon::setUsageMapper(
     mUsageMapperLru.clear();
     mUsageMapper = mapper;
     Traits traits = {
-        mTraits->name, mTraits->id, LINEAR,
+        mTraits ? mTraits->name : "", mTraits ? mTraits->id : C2Allocator::id_t {}, LINEAR,
         C2MemoryUsage(minUsage), C2MemoryUsage(maxUsage)
     };
     mTraits = std::make_shared<Traits>(traits);
@@ -508,9 +518,9 @@ c2_status_t C2AllocatorIon::newLinearAllocation(
     }
 
     allocation->reset();
-    if (mInit != C2_OK) {
-        return mInit;
-    }
+    // if (mInit != C2_OK) {
+    //     return mInit;
+    // }
 
     size_t align = 0;
     unsigned heapMask = ~0;
@@ -521,7 +531,7 @@ c2_status_t C2AllocatorIon::newLinearAllocation(
     }
 
     std::shared_ptr<C2AllocationIon> alloc
-        = std::make_shared<C2AllocationIon>(dup(mIonFd), capacity, align, heapMask, flags, mTraits->id);
+        = std::make_shared<C2AllocationIon>(dup(mIonFd), capacity, align, heapMask, flags, mTraits ? mTraits->id : C2Allocator::id_t {} );
     ret = alloc->status();
     if (ret == C2_OK) {
         *allocation = alloc;
