87e309288f0508c4dade1452c2ef0202321ceec4
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2018-06-29 20:07:06 +0300

    [c2][test] Fix: pthread_mutex_destroy called on a destroyed mutex
    Issue: MDP-43085
    
    Test: unittests don't cause FORTIFY message anymore
    
    Change-Id: I60881b5a22ef3f91a126966a0e22f06368ac4b2e

--- c2_utils/src/mfx_debug.cpp
+++ c2_utils/src/mfx_debug.cpp
@@ -1,6 +1,6 @@
 /**********************************************************************************
 
-Copyright (C) 2005-2016 Intel Corporation.  All rights reserved.
+Copyright (C) 2005-2018 Intel Corporation.  All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
@@ -333,9 +333,9 @@ void MfxTraceable::Unregister(void* instance)
     names_.erase(instance);
 }
 
-std::mutex MfxTraceable::mutex_;
+MFX_HIDE std::mutex MfxTraceable::mutex_;
 
-std::unordered_map<void*, const char*> MfxTraceable::names_;
+MFX_HIDE std::unordered_map<void*, const char*> MfxTraceable::names_;
 
 #endif // #if MFX_DEBUG == MFX_DEBUG_YES
 
