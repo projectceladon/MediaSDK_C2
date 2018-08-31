0bdd11c1fe90a2bd8c307a0fe7826909399d990c
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2018-08-31 15:05:57 +0300

    [c2][build] Fails when device.mk configured to mediasdk open source
    Issue: MDP-48176
    
    Test: open/closed source build
    
    Change-Id: I8a05dab84582105339a353b7e2fc9638379ab845

--- mfx_c2_env.mk
+++ mfx_c2_env.mk
@@ -7,16 +7,16 @@ ifeq ($(MFX_C2_IMPL_HW),)
   MFX_C2_IMPL_HW:=true
 endif
 
-# Build SW C2 plugins
-ifneq ($(BOARD_HAVE_MEDIASDK_OPEN_SOURCE),true)
+ifeq ($(BOARD_HAVE_MEDIASDK_SRC),true)
+
+  # Build SW C2 plugins
   ifeq ($(MFX_C2_IMPL_SW),)
     MFX_C2_IMPL_SW:=true
   endif
-endif
 
-# Build PURE C2 plugins
-ifneq ($(BOARD_HAVE_MEDIASDK_OPEN_SOURCE),true)
+  # Build PURE C2 plugins
   ifeq ($(MFX_C2_IMPL_PURE),)
     MFX_C2_IMPL_PURE:=true
   endif
+
 endif
