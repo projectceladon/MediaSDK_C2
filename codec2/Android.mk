a10395d668732f6049dc444fda9a9ea4bdac964d
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2018-10-25 13:30:35 +0300

    [c2][vndk] c2 headers/vndk update ww43.4, 9dc40ef37443af1fca89ae7ad73818aca3050402
    Issue: MDP-51296
    
    Test: not run
    
    Change-Id: Ie280e85336bc33cdea9609f7ab44031eeb4f0363

--- codec2/Android.mk
+++ codec2/Android.mk
@@ -10,6 +10,7 @@ C2_OUT_TEMP := $(PRODUCT_OUT)/gen/ETC/Codec2-docs_intermediates
 C2_DOXY := $(or $(shell command -v doxygen),\
 		$(shell command -v /Applications/Doxygen.app/Contents/Resources/doxygen))
 
+.PHONY: check-doxygen
 check-doxygen:
 ifndef C2_DOXY
 	$(error 'doxygen is not available')
@@ -27,18 +28,21 @@ $(C2_OUT_TEMP)/doxy-internal.config: $(C2_ROOT)/docs/doxygen.config
 	sed 's:\(^OUTPUT_DIRECTORY *= \)out\(.*\)api:\1'$(OUT_DIR)'\2internal:;' \
 		$(C2_ROOT)/docs/doxygen.config > $@
 
+.PHONY: docs-api
 docs-api: $(C2_OUT_TEMP)/doxy-api.config check-doxygen
 	echo API docs are building in $(C2_DOCS_ROOT)/api
 	rm -rf $(C2_DOCS_ROOT)/api
 	mkdir -p $(C2_DOCS_ROOT)/api
 	$(C2_DOXY) $(C2_OUT_TEMP)/doxy-api.config
 
+.PHONY: docs-internal
 docs-internal: $(C2_OUT_TEMP)/doxy-internal.config check-doxygen
 	echo Internal docs are building in $(C2_DOCS_ROOT)/internal
 	rm -rf $(C2_DOCS_ROOT)/internal
 	mkdir -p $(C2_DOCS_ROOT)/internal
 	$(C2_DOXY) $(C2_OUT_TEMP)/doxy-internal.config
 
+.PHONY: docs-all
 docs-all: docs-api docs-internal
 
 include $(call all-makefiles-under,$(call my-dir))
