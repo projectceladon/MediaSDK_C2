8ccc69a04ac1ec724a4dc61e9a57768b7b93913e
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2019-01-11 13:18:53 +0300

    [c2] Update copyright to 2017-2019
    Issue: N/A
    
    Test: Existing
    
    Change-Id: I939e9b89425251724aff82ebe566cb478ef871e1

--- c2_utils/src/mfx_c2_debug.cpp
+++ c2_utils/src/mfx_c2_debug.cpp
@@ -1,6 +1,6 @@
 /**********************************************************************************
 
-Copyright (C) 2005-2018 Intel Corporation.  All rights reserved.
+Copyright(c) 2005-2019 Intel Corporation. All Rights Reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
