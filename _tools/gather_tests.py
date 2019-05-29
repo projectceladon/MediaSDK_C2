b0d603d3da79ebcce069225606bd9d285c599636
Dulaev, AlexeyX <alexeyx.dulaev@intel.com>
2019-05-29 14:48:19 +0300

    [c2][tools] Update gathering test list
    Needs an update after parametric GTEST tests were introduced.
    
    Issue: None
    
    Test: Existing
    
    Change-Id: I71388f8a2a80062e947a028d5a3f9c511f7c8089

--- _tools/gather_tests.py
+++ _tools/gather_tests.py
@@ -1,3 +1,13 @@
+# /********************************************************************************
+#
+# INTEL CORPORATION PROPRIETARY INFORMATION
+# This software is supplied under the terms of a license agreement or nondisclosure
+# agreement with Intel Corporation and may not be copied or disclosed except in
+# accordance with the terms of that agreement
+# Copyright(c) 2017-2019 Intel Corporation. All Rights Reserved.
+#
+# *********************************************************************************/
+
 # Produces html report file with description tables of all gtest tests
 # found in specified folder.
 # Usage: python _tools/gather_tests.py -i unittests/src -o tests.html
@@ -6,8 +16,9 @@ from __future__ import print_function  # Only needed for Python 2
 import os
 import re
 
-re_test = re.compile("TEST\((.*), (.*)\)")
-re_comment = re.compile("// (.*) ?")
+re_test = re.compile("TEST(?:_P)?\((.*), (.*)\)")
+re_decode_conditions = re.compile('DecodingConditions\(\"(.*)\"\)')
+re_comment = re.compile(" *// (.*) ?")
 
 import argparse
 
@@ -49,13 +60,17 @@ def FilterRows(src, condition):
     return dst
 
 tests = []
+decode_conditions = []
+
 for root, dirs, filenames in os.walk(args.test_src_dir):
     for f in filenames:
         with open(os.path.join(root, f),'r') as src:
             lines = src.readlines()
             for i in range(len(lines)):
                 m = re_test.match(lines[i])
-                if m:
+                if not m:
+                    decode_condition = re_decode_conditions.search(lines[i])
+                if m or decode_condition:
                     comment = ""
                     for j in range(i - 1, 1, -1):
                         comment_match = re_comment.match(lines[j])
@@ -66,17 +81,22 @@ for root, dirs, filenames in os.walk(args.test_src_dir):
                                 comment = comment_match.group(1)
                         else:
                             break
-                    tests.append((m.group(1), m.group(2), comment))
 
-store_tests = FilterRows(tests, lambda row: row[0] == "MfxComponentStore")
-decoder_tests = FilterRows(tests, lambda row: row[0] == "MfxDecoderComponent")
-encoder_tests = FilterRows(tests, lambda row: row[0] == "MfxEncoderComponent")
+                    if m:
+                        tests.append((m.group(1), m.group(2), comment))
+                    else:
+                        decode_conditions.append((decode_condition.group(1), comment))
+
+store_tests = FilterRows(tests, lambda row: row[0] in ["MfxComponentStore", "MfxC2Service"] )
+decoder_tests = FilterRows(tests, lambda row: "Decoder" in row[0])
+decoder_tests += [('DecoderDecode', 'Check/' + dc[0], dc[1]) for dc in decode_conditions]
+encoder_tests = FilterRows(tests, lambda row: "Encoder" in row[0])
 
 columns = ('Testable', 'Test', 'Description')
 
 with open(args.html_output, 'w') as dst:
     HtmlBegin(dst)
-    HtmlHeading('C2 Store Tests')
+    HtmlHeading('C2 Store and Service Tests')
     HtmlTable(columns, store_tests, dst)
     HtmlHeading('C2 Mock Component and Utility Library Tests')
     HtmlTable(columns, tests, dst)
