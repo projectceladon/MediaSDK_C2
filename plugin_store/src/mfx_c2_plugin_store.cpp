3f06cb7e5e65fd4cdd285c4df056f3877f1212a7
Yang, Dong <dong.yang@intel.com>
2021-02-25 14:43:49 +0800

    [c2][plugin] implement mfx c2 BufferQueue
    c2 buffer queue implementation, c2 plugin will use the buffer queue
    for graphic's memory allocate/free.
    
    Change-Id: Ifcb03356d8ddd80c6399befd3a1d95ea2ee129e7
    Signed-off-by: Yang, Dong <dong.yang@intel.com>

---
+++ plugin_store/src/mfx_c2_plugin_store.cpp
@@ -0,0 +1,88 @@
+/********************************************************************************
+
+INTEL CORPORATION PROPRIETARY INFORMATION
+This software is supplied under the terms of a license agreement or nondisclosure
+agreement with Intel Corporation and may not be copied or disclosed except in
+accordance with the terms of that agreement
+Copyright(c) 2021 Intel Corporation. All Rights Reserved.
+
+*********************************************************************************/
+#include <inttypes.h>
+#include <map>
+#include <memory>
+#include <mutex>
+
+#include <C2AllocatorGralloc.h>
+
+#include "mfx_c2_buffer_queue.h"
+#include "mfx_c2_allocator_id.h"
+#include "mfx_debug.h"
+#include "mfx_c2_debug.h"
+
+namespace android {
+
+C2Allocator* createAllocator(C2Allocator::id_t allocatorId) {
+    MFX_DEBUG_TRACE_FUNC;
+    MFX_DEBUG_TRACE_PRINTF("%s(): Fallback to create C2AllocatorGralloc (id=%u)", __func__, allocatorId);
+    return new C2AllocatorGralloc(allocatorId, true);
+}
+
+std::shared_ptr<C2Allocator> fetchAllocator(C2Allocator::id_t allocatorId) {
+    MFX_DEBUG_TRACE_FUNC;
+    MFX_DEBUG_TRACE_PRINTF("%s(allocatorId=%d)", __func__, allocatorId);
+    static std::mutex sMutex;
+    static std::map<C2Allocator::id_t, std::weak_ptr<C2Allocator>> sCacheAllocators;
+
+    std::lock_guard<std::mutex> lock(sMutex);
+
+    std::shared_ptr<C2Allocator> allocator;
+    auto iter = sCacheAllocators.find(allocatorId);
+    if (iter != sCacheAllocators.end()) {
+        allocator = iter->second.lock();
+        if (allocator != nullptr) {
+            return allocator;
+        }
+    }
+
+    allocator.reset(createAllocator(allocatorId));
+    sCacheAllocators[allocatorId] = allocator;
+    return allocator;
+}
+
+C2BlockPool* createBlockPool(C2Allocator::id_t allocatorId, C2BlockPool::local_id_t poolId) {
+    MFX_DEBUG_TRACE_FUNC;
+    MFX_DEBUG_TRACE_PRINTF("%s(allocatorId=%d, poolId=%" PRIu64 ")", __func__, allocatorId, poolId);
+
+    std::shared_ptr<C2Allocator> allocator = fetchAllocator(allocatorId);
+    if (allocator == nullptr) {
+        MFX_DEBUG_TRACE_PRINTF("%s(): Failed to create allocator id=%u", __func__, allocatorId);
+        return nullptr;
+    }
+
+    switch (allocatorId) {
+    case MFX_BUFFERQUEUE:
+        MFX_DEBUG_TRACE_PRINTF("%s(new MfxC2BufferQueueBlockPool allocatorId=%d, poolId=%" PRIu64 ")", __func__, allocatorId, poolId);
+        return new MfxC2BufferQueueBlockPool(allocator, poolId);
+    default:
+        MFX_DEBUG_TRACE_PRINTF("%s(): Unknown allocator id=%u", __func__, allocatorId);
+        return nullptr;
+    }
+}
+
+}  // namespace android
+
+extern "C" ::C2BlockPool* CreateBlockPool(::C2Allocator::id_t allocatorId,
+                                          ::C2BlockPool::local_id_t poolId) {
+    MFX_DEBUG_TRACE_FUNC;
+    MFX_DEBUG_TRACE_PRINTF("%s(allocatorId=%d, poolId=%" PRIu64 ")", __func__, allocatorId, poolId);
+    return ::android::createBlockPool(allocatorId, poolId);
+}
+
+extern "C" ::C2Allocator* CreateAllocator(::C2Allocator::id_t allocatorId, ::c2_status_t* status) {
+    MFX_DEBUG_TRACE_FUNC;
+    MFX_DEBUG_TRACE_PRINTF("%s(allocatorId=%d)", __func__, allocatorId);
+
+    ::C2Allocator* res = ::android::createAllocator(allocatorId);
+    *status = (res != nullptr) ? C2_OK : C2_BAD_INDEX;
+    return res;
+}
